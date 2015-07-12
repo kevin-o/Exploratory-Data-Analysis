0017428e67279deb6ff5b8c4cb24a4a73a57f45a
kevino <kaolaughlin@verizon.net>
2015-07-12 08:15:28 -0400

    updated legends

--- Plot3.R
+++ Plot3.R
@@ -1,4 +1,6 @@
-
+## Plot 3
+library(dplyr)
+## assumes the working directory contains the datafile
 data.df <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";" , na.strings = "?")
 ## change Date and Time factor variables to datetime variable
 
@@ -10,16 +12,31 @@ data.df$datetime <- strptime(paste(data.df$Date,data.df$Time), format = "%Y-%m-%
 ## add day of week to dataframe
 data.df$Day <- weekdays(data.df$datetime)
 
+## set up canvas for plotting
 par(mfrow = c(1,1))
-
 
 ## plot the sub metering data
 plot(data.df$datetime, data.df$Sub_metering_1, type = "l", main = "", xlab = "", ylab = "Energy sub metering")
 lines(data.df$datetime, data.df$Sub_metering_2, col = "red")
 lines(data.df$datetime, data.df$Sub_metering_3, col = "blue")
 ## now create the legend
-legend("topright",inset = .05, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1), col=c("black","red","blue"),pt.cex = 1, cex = .5)
-## and finally send to a .png file
-dev.copy(png, file = "plot3.png",width = 800, height = 600,
-         pointsize = 10)
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1), col=c("black","red","blue"),  cex = .9, xjust = 0)
+
+## had trouble getting the plot to png file with the following code in Rstudio as the text in the legend was being cut off...
+##dev.copy(png, file = "plot3.png",width = 800, height = 600,
+##        pointsize = 8)
+##dev.off()
+
+## ... so solved the problem with and finally send to a .png file
+png(file = "plot3.png", width = 800, height = 600)
+        ## set up canvas for plotting
+        par(mfrow = c(1,1))
+
+        ## plot the sub metering data
+        plot(data.df$datetime, data.df$Sub_metering_1, type = "l", main = "", xlab = "", ylab = "Energy sub metering")
+        lines(data.df$datetime, data.df$Sub_metering_2, col = "red")
+        lines(data.df$datetime, data.df$Sub_metering_3, col = "blue")
+        ## now create the legend
+        legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1), col=c("black","red","blue"),  cex = .9, xjust = 0)
 dev.off()
+
--- Plot4.R
+++ Plot4.R
@@ -1,4 +1,6 @@
-
+## Plot 4
+library(dplyr)
+## assumes data file resides in current working directory
 data.df <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";" , na.strings = "?")
 
 ## change Date variable to Date format
@@ -15,28 +17,28 @@ data.df$datetime <- strptime(paste(data.df$Date,data.df$Time), format = "%Y-%m-%
 data.df$Day <- weekdays(data.df$datetime)
 
 
+png(file = "plot4.png", width = 800, height = 600)
+        #create four plotting areas on the canvas
+        par(mfrow = c(2,2))
 
-#create four plotting areas on the canvas
-par(mfrow = c(2,2))
+        # plot the first quadrant
+        plot(data.df$datetime, data.df$Global_active_power, type = "l", main = "", xlab = "", ylab = "Global Active Power (kilowatts)")
 
-# plot the first quadrant
-plot(data.df$datetime, data.df$Global_active_power, type = "l", main = "", xlab = "", ylab = "Global Active Power (kilowatts)")
+        #plot the upper right quadrant
+        plot(data.df$datetime, data.df$Voltage, type = "l", main = "", xlab = "", ylab = "Voltage")
 
-#plot the upper right quadrant
-plot(data.df$datetime, data.df$Voltage, type = "l", main = "", xlab = "", ylab = "Voltage")
+        #plot the lower left quadrant
+        plot(data.df$datetime, data.df$Sub_metering_1, type = "l", main = "", xlab = "", ylab = "Energy sub metering")
+        lines(data.df$datetime, data.df$Sub_metering_2, col = "red")
+        lines(data.df$datetime, data.df$Sub_metering_3, col = "blue")
 
-#plot the lower left quadrant
-plot(data.df$datetime, data.df$Sub_metering_1, type = "l", main = "", xlab = "", ylab = "Energy sub metering")
-lines(data.df$datetime, data.df$Sub_metering_2, col = "red")
-lines(data.df$datetime, data.df$Sub_metering_3, col = "blue")
+        ## now create the legend
+        legend("topright",inset = .1, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1), col=c("black","red","blue"), bty = "n", pt.cex = 1, cex = .8)
 
-## now create the legend
-legend("topright",inset = .1, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1), col=c("black","red","blue"), bty = "n", pt.cex = 1, cex = .8)
-
-## plot the lower right quadrant
-plot(data.df$datetime, data.df$Global_reactive_power, type = "l", main = "", xlab = "", ylab = "Global Reactive Power")
-
-## and finally send to a .png file
-dev.copy(png, file = "plot4.png", width = 800, height = 600,
-         pointsize = 12)
+        ## plot the lower right quadrant
+        plot(data.df$datetime, data.df$Global_reactive_power, type = "l", main = "", xlab = "", ylab = "Global Reactive Power")
 dev.off()
+## and finally send to a .png file
+## dev.copy(png, file = "plot4.png", width = 800, height = 600,
+ ##        pointsize = 12)
+## dev.off()
